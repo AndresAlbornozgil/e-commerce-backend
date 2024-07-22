0724e50f5838e7fcf78834e011c8cda301598a56
AA <aalborgil002@gmail.com>
2024-07-22 16:56:16 -0400

    Added db, models

--- Develop/routes/api/category-routes.js
+++ Develop/routes/api/category-routes.js
@@ -6,6 +6,13 @@ const { Category, Product } = require('../../models');
 router.get('/', (req, res) => {
   // find all categories
   // be sure to include its associated Products
+  try {
+    Category.findAll().then((data) => {
+      res.json(data)
+    })
+  } catch (error) {
+    
+  }
 });
 
 router.get('/:id', (req, res) => {
