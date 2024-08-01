9995ce6aef0d384ef42136025bfd275f23ad882d
AA <aalborgil002@gmail.com>
2024-08-01 18:31:46 -0400

    Added final code

--- Develop/config/connection.js
+++ Develop/config/connection.js
@@ -2,6 +2,8 @@ require('dotenv').config();
 
 const Sequelize = require('sequelize');
 
+// console.log("sql", Sequelize)
+
 const sequelize = process.env.DB_URL
   ? new Sequelize(process.env.DB_URL)
   : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PASSWORD, {
